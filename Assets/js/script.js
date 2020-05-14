2f98088e13caf2e30bd4c4f8434bbe5c7a53eae2
George Flores <georgeflores886@gmail.com>
2020-05-13 20:00:32 -0400

    adjusted UI

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -29,19 +29,20 @@ searchBtn.on("click", function (event) {
 });
 //current weather function
 function currentForecast(cityName) {
+    //creating today's day for current weather 
+    var d = new Date();
+    var strDate = (d.getMonth() + 1) + "/" + d.getDate()  + "/"  + d.getFullYear();
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0888bb26c1d027c60cb2417244156801";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
-        var currCard = $("<div>").addClass("card")//card body for current daily forecast
+        console.log(response)
+        var currCard = $("<div>").addClass("card current-day")//card body for current daily forecast
         $("#dailyforecast").append(currCard);
-        // var row = $('<div>').addClass('row no-gutters');
-        // currCard.append(row);
 
-        var title = $('<h2>').addClass('card-title').text(response.name); //city name
+        var title = $('<h2>').addClass('card-title').text(response.name + " " + strDate); //city name and current date
         currCard.append(title);
 
         var icon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png"; //grabbing url for image so that it displays in card
@@ -79,8 +80,9 @@ function currentForecast(cityName) {
             } else {
                 changeColor = "red"
             }
+            //need to append this properly
             var uvItem = $("<p>").addClass("card-text current-uv").text("UV Index: ")
-            uvItem.append($("<span>").attr("style", ("background-color:" + changeColor)).text(index));
+            uvItem.append($("<span>").css("background-color:" + changeColor)).text(index);
             currCard.append(uvItem);
         });
     });
@@ -98,19 +100,18 @@ function weeklyForecast(cityName) {
             var divRow = $("<div>").addClass("row");
             $("#dailyforecast").append(divRow);
 
-        //creating cards through loop for 5 day
+        //creating cards through loop
         for (var i = 0; i < 5; i++) {
             
-
-            //creating cards for the 5 days
-            var dayCard = $("<div>").addClass("card col-2");
-            var cardBody = $("<div>").addClass("card-body")
+            //creating cards for days
+            var dayCard = $("<div>").addClass("card dayCard col-2 bg-primary");
+            var cardBody = $("<div>").addClass("card-body text-white")
 
             divRow.append(dayCard);
 
             dayCard.append(cardBody)
             //creating variables to store date, temp, icon, and humidity and appending them to cards
-            var forecastDate = $("<h4>").addClass("card-header city").text(response.list[i].dt_txt.slice(0, 10)); //take the first 10 characters from dt_txt
+            var forecastDate = $("<h4>").addClass("card-title").text(response.list[i].dt_txt.slice(0, 10)); //take the first 10 characters from dt_txt
             cardBody.append(forecastDate);
 
             var iconURL = "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png";
@@ -121,18 +122,20 @@ function weeklyForecast(cityName) {
             var forecastTemp = $("<p>").addClass("card-text").html("Temp: " + convertTemp.toFixed(2) + "&#730F");
             cardBody.append(forecastTemp);
 
-            var forecastHum = $("<p>").addClass("card-text").text("Humidity: " + response.list[i].main.humidity);
+            var forecastHum = $("<p>").addClass("card-text").text("Humidity: " + response.list[i].main.humidity + "%");
             cardBody.append(forecastHum);
 
             // $("#dailyforecast").append(dayCard); //append cards to dailyforecast so it appears below current forecast
-
         }
     });
 }
-
-//clear div after clicking new search
+//clear div after clicking searchBtn
 function clearAll() {
     $("#dailyforecast").empty();
 }
 
-$(document).on("click", ".city", currentForecast, weeklyForecast);
+$(document).on("click", ".city", function () {
+    thisCity = $(this).text();
+    currentForecast(thisCity);
+    weeklyForecast(thisCity);
+})
